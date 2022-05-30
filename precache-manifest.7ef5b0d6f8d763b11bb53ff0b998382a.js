652eaa48504f0b196c365078281f60345f65cb02
willem <1121567132@qq.com>
2022-05-30 12:43:27 +0000

    add gh-pages

---
+++ precache-manifest.7ef5b0d6f8d763b11bb53ff0b998382a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ff01b9ce0b9e7f7f1ca391c6c4e7852",
+    "url": "/1.png"
+  },
+  {
+    "revision": "1fb748109cd8db2c0e2761ae1b691171",
+    "url": "/2.png"
+  },
+  {
+    "revision": "98e618134784d8e033f1efe01f033adc",
+    "url": "/3.png"
+  },
+  {
+    "revision": "d6d70cf7556f34271e5e1d948a444b55",
+    "url": "/Main.vue"
+  },
+  {
+    "revision": "c7ada07b90a4938f834c",
+    "url": "/css/app.502893ab.css"
+  },
+  {
+    "revision": "d90082bd7c70fe677ed1",
+    "url": "/css/chunk-vendors.43146d95.css"
+  },
+  {
+    "revision": "31d47085569e772c0f57aa8ec8381a5c",
+    "url": "/fonts/materialdesignicons-webfont.31d47085.woff"
+  },
+  {
+    "revision": "4a837d054b5f2a37170df8a275a13816",
+    "url": "/fonts/materialdesignicons-webfont.4a837d05.eot"
+  },
+  {
+    "revision": "b0fd91bb29dcb296a9a37f8bda0a2d85",
+    "url": "/fonts/materialdesignicons-webfont.b0fd91bb.ttf"
+  },
+  {
+    "revision": "f1997a8aba8a498fe4032e3b56e871ca",
+    "url": "/fonts/materialdesignicons-webfont.f1997a8a.woff2"
+  },
+  {
+    "revision": "169680e2a4b888af0dc69f54e9a77524",
+    "url": "/img/vercel/dark.svg"
+  },
+  {
+    "revision": "a65a45dfb17a1f02b8e3b9dff95c170b",
+    "url": "/img/vercel/light.svg"
+  },
+  {
+    "revision": "76e938d743dda4f8f9104c9b13856ca9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7ada07b90a4938f834c",
+    "url": "/js/app.fd01618f.js"
+  },
+  {
+    "revision": "d90082bd7c70fe677ed1",
+    "url": "/js/chunk-vendors.94947525.js"
+  },
+  {
+    "revision": "61b28792c46c00028f8a91dd4671538e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
